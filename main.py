55b262d1a43b3a229177159f3e5e6a7a79a5d58b
Ismail Han <im087921@gmail.com>
2024-03-15 02:14:59 +0600

    complete

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import sys
+import threading
+
 from PyQt5.QtWidgets import QApplication, QWidget, QComboBox, QVBoxLayout
 import PyQt5
 import os,re
 from PyQt5.QtCore import Qt
+import concurrent.futures
 
+from threading import Thread
+n=0
+lennn = 0
 def partition_list():
     regex = r"([^\\s]*:)"
     driver = os.popen("wmic logicaldisk get name").read()
@@ -14,7 +20,7 @@ def partition_list():
     return partition_list
 def file_list(drive):
     user_name = os.getlogin()
-    path = f"{drive}:\\Users\\{user_name}\\AppData\\"
+    path = f"{drive}:\\Users\\{user_name}\\AppData\\test"
 
     def convert_bytes_to_gb(bytes):
         gb = bytes / (1024 ** 3)  # 1024 bytes = 1 kilobyte, 1024 kilobytes = 1 megabyte, 1024 megabytes = 1 gigabyte
@@ -38,15 +44,23 @@ def file_list(drive):
         c_drive_directory = path
         files = list_files_by_size(c_drive_directory)
 
-        print("Files on C drive sorted by size:")
+        # print("Files on C drive sorted by size:")
         for file, size in files:
             size_gb = convert_bytes_to_gb(size)
             # print(f"{file}: {size_gb:.2f} GB")
+        global lennn
+        lennn = len(files)
         return files
 all_files = []
 def scan_button():
     selected_drive = my_combobox.currentText()
-    file_name = file_list(str(selected_drive))
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        future = executor.submit(file_list, str(selected_drive))
+        return_value = future.result()
+        file_name = return_value
+
+    # print(file_name)
+    # file_name = file_list(str(selected_drive))
     count = 0
     for trans_one in reversed(file_name):
         for trans_two in trans_one:
@@ -64,20 +78,35 @@ def scan_button():
         count = 0
 def move():
     global all_files
-
-    # print(all_files)
+    moved_path = path_select.currentText()
+    print(moved_path)
     for new in range(path.count()):
         # print(new)
         itemm = path.item(new)
-        # print(itemm)
+
         if itemm.checkState() == Qt.CheckState():
             pass
         else:
-            print(all_files[new])
+            # print(all_files[new])
+            new_path_one_with_new_drive_added =str(moved_path) +(str(all_files[new]))[1:]
+            print(new_path_one_with_new_drive_added)
+
+            def remove_last_element(path):
+                # Split the path into a list of elements
+                path_elements = path.split(os.sep)
+                # Remove the last element from the list
+                path_elements.pop()
+                # Join the elements back into a path
+                directory_path = os.sep.join(path_elements)
+                return directory_path
+            parent_path_with_new_drive_added = remove_last_element(new_path_one_with_new_drive_added)
+            parent_path_old = remove_last_element(str(all_files[new]))
+            command = f"mklink /D {parent_path_with_new_drive_added} {parent_path_old}"
+            pass
         # if PyQt5.QtWidgets.QListWidgetItem(new).checkState() == Qt.Checked:
         #     print(new)
 
-        pass
+
 # Create a QApplication instance
 app = QApplication(sys.argv)
 
@@ -93,6 +122,9 @@ drives = partition_list()
 my_combobox.addItems(drives)
 scan = PyQt5.QtWidgets.QPushButton("Scan")
 scan.clicked.connect(scan_button)
+progress_bar = PyQt5.QtWidgets.QProgressBar()
+progress_bar.setMinimum(0)
+progress_bar.setMaximum(lennn)
 path = PyQt5.QtWidgets.QListWidget()
 
 size = PyQt5.QtWidgets.QListWidget()
@@ -111,6 +143,7 @@ layout = QVBoxLayout()
 
 layout.addWidget(my_combobox)
 layout.addWidget(scan)
+layout.addWidget(progress_bar)
 
 layout.addWidget(spliter)
 layout.addWidget(path_select)
